5c075ac7d93eb7bb39c76b54c160eb6e2f2328b7
Trevor Strieber <trevor@usestable.com>
2025-08-07 08:27:55 -0700

    Initial Commit

---
+++ Docket/DocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocketApp.swift
+//  Docket
+//
+//  Created by Trevor Strieber on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct DocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DocketTests/DocketTests.swift
@@ -0,0 +1,17 @@
+//
+//  DocketTests.swift
+//  DocketTests
+//
+//  Created by Trevor Strieber on 8/7/25.
+//
+
+import Testing
+@testable import Docket
+
+struct DocketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
